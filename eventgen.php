166e4e4ed529af0bd315adeb41bb806941005e9e
JIkun Zhu <jikun.zhu@gmail.com>
2013-05-06 04:03:57 -0400

    test

--- eventgen.php
+++ eventgen.php
@@ -47,7 +47,7 @@ if ($user_id) {
   }
 }
 
-$friends = idx($facebook->api('/me/friends?limit=100'), 'data', array());
+$friends = idx($facebook->api('/me/friends?limit=10'), 'data', array());
 
 ?>
 
