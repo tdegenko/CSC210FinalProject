852dc9f2190c5c1f58561499e77f8d2a571fc0f2
JIkun Zhu <jikun.zhu@gmail.com>
2013-05-06 05:53:00 -0400

    test

--- eventgen.php
+++ eventgen.php
@@ -131,15 +131,15 @@ $app_using_friends = $facebook->api(array(
 	        
 	        
 	          <?php
-	             $w = 0;
+	             //$w = 0;
 	            foreach ($app_using_friends as $auf) {
 	              // Extract the pieces of info we need from the requests above
 	              $id = idx($auf, 'uid');
 	              $name = idx($auf, 'name');
-	              $w++;
-	              if ($w%5==0){
-					echo "<br>;"
-					}
+	              //$w++;
+	              //if ($w%5==0){
+					//echo "<br>;"
+					//}
 					
 					
 	          ?>
