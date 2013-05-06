bf3f7e0fd0e7028ca4fe7d1599603e91457a8ccf
JIkun Zhu <jikun.zhu@gmail.com>
2013-05-06 03:44:23 -0400

    test

--- eventgen.php
+++ eventgen.php
@@ -47,7 +47,7 @@ if ($user_id) {
   }
 }
 
-$friends = idx($facebook->api('/me/friends'), 'data', array());
+//$friends = idx($facebook->api('/me/friends'), 'data', array());
 
 $app_using_friends = $facebook->api(array(
     'method' => 'fql.query',
@@ -146,26 +146,7 @@ $app_using_friends = $facebook->api(array(
 	      </div>
 	
 	
-		<div class="list">
-	        <h3>A few of your friends</h3>
-	        <ul class="friends">
-	          <?php
-	            foreach ($friends as $friend) {
-	              // Extract the pieces of info we need from the requests above
-	              $id = idx($friend, 'id');
-	              $name = idx($friend, 'name');
-	          ?>
-	          <li>
-	            <a href="https://www.facebook.com/<?php echo he($id); ?>" target="_top">
-	              <img src="https://graph.facebook.com/<?php echo he($id) ?>/picture?type=square" alt="<?php echo he($name); ?>">
-	              <?php echo he($name); ?>
-	            </a>
-	          </li>
-	          <?php
-	            }
-	          ?>
-	        </ul>
-	      </div>
+
 <!--    ==================OLD VERSION==================
     </div>
     <form enctype="multipart/form-data" action="<?php echo $event_url; ?>" method="post">
