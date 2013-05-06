e3439b6e76ae44d4c7930fb12171f8af0a8790a9
JIkun Zhu <jikun.zhu@gmail.com>
2013-05-06 03:40:10 -0400

    test

--- eventgen.php
+++ eventgen.php
@@ -146,23 +146,20 @@ $app_using_friends = $facebook->api(array(
 	      </div>
 	
 	
-		<div class="list inline">
-	        <h3>Friends</h3>
-	        <ul class="photos">
+		<div class="list">
+	        <h3>A few of your friends</h3>
+	        <ul class="friends">
 	          <?php
-	            $i = 0;
 	            foreach ($friends as $friend) {
 	              // Extract the pieces of info we need from the requests above
 	              $id = idx($friend, 'id');
 	              $name = idx($friend, 'name');
-
-	              $class = ($i++ % 4 === 0) ? 'first-column' : '';
 	          ?>
 	          <li>
 	            <a href="https://www.facebook.com/<?php echo he($id); ?>" target="_top">
 	              <img src="https://graph.facebook.com/<?php echo he($id) ?>/picture?type=square" alt="<?php echo he($name); ?>">
 	              <?php echo he($name); ?>
-	</a>
+	            </a>
 	          </li>
 	          <?php
 	            }
