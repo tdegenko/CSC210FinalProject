7209fc35266ee17b95ed5712ae633f5646e4804d
JIkun Zhu <jikun.zhu@gmail.com>
2013-05-06 03:48:39 -0400

    test

--- eventgen.php
+++ eventgen.php
@@ -47,6 +47,8 @@ if ($user_id) {
   }
 }
 
+$friends = idx($facebook->api('/me/friends'), 'data', array());
+
 ?>
 
 
@@ -116,6 +118,31 @@ if ($user_id) {
         <p><input type="submit" value="Create Event" /></p>
     </form>
     <div>
+	
+		<div class="list">
+	        <h3>A few of your friends</h3>
+	        <ul class="friends">
+	          <?php
+	            foreach ($friends as $friend) {
+	              // Extract the pieces of info we need from the requests above
+	              $id = idx($friend, 'id');
+	              $name = idx($friend, 'name');
+	          ?>
+	          <li>
+	            <a href="https://www.facebook.com/<?php echo he($id); ?>" target="_top">
+	              <img src="https://graph.facebook.com/<?php echo he($id) ?>/picture?type=square" alt="<?php echo he($name); ?>">
+	              <?php echo he($name); ?>
+	            </a>
+	          </li>
+	          <?php
+	            }
+	          ?>
+	        </ul>
+	      </div>
+	
+	
+	
+	
 <!--    ==================OLD VERSION==================
     </div>
     <form enctype="multipart/form-data" action="<?php echo $event_url; ?>" method="post">
