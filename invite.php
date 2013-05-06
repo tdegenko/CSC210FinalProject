89472e7aee2a0816f699e312b0edc014c4361829
JIkun Zhu <jikun.zhu@gmail.com>
2013-05-06 08:16:59 -0400

    test

--- invite.php
+++ invite.php
@@ -42,8 +42,8 @@ $eids = $facebook->api(array(
   ));
 
 
-  $e_id = $eids[1];
-  echo "$eids";
+  $eid = idx($eids, 'eid');
+  echo "$eid[0]";
   //$e_details = $facebook -> api("/{$e_id}");
   //echo "$e_details";
   $att = $_REQUEST["attend"];
