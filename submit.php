2833b86e2fffa4d5a3736a1f8e6885db10d17f09
JIkun Zhu <jikun.zhu@gmail.com>
2013-05-06 16:13:55 -0400

    test

--- submit.php
+++ submit.php
@@ -1,5 +1,8 @@
 <?php
 
+
+
+
 $url=parse_url(getenv("CLEARDB_DATABASE_URL"));
 
     $server = $url["host"];
@@ -20,7 +23,44 @@ if (!$select){
 	die ("Unable to connect database");
 }
 
+require_once('AppInfo.php');
+// Enforce https on production
+if (substr(AppInfo::getUrl(), 0, 8) != 'https://' && $_SERVER['REMOTE_ADDR'] != '127.0.0.1') {
+  header('Location: https://'. $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+  exit();
+}
 
+// This provides access to helper functions defined in 'utils.php'
+require_once('utils.php');
+require_once('sdk/src/facebook.php');
+
+$facebook = new Facebook(array(
+  'appId'  => AppInfo::appID(),
+  'secret' => AppInfo::appSecret(),
+  'sharedSession' => true,
+  'trustForwarded' => true,
+));
+
+$access_token = $_SESSION['token'];
+$facebook->setAccessToken($access_token);
+
+$user_id = $facebook->getUser();
+if ($user_id) {
+   
+  try {
+    // Fetch the viewer's basic information
+    $basic = $facebook->api('/me');
+  } catch (FacebookApiException $e) {
+    // If the call fails we check if we still have a user. The user will be
+    // cleared if the error is because of an invalid accesstoken
+    if (!$facebook->getUser()) {
+      header('Location: '. AppInfo::getUrl($_SERVER['REQUEST_URI']));
+      exit();
+    }
+  }
+}
+
+echo "user_id";
 
 //Don't know how id is represented
 $id = 1;
@@ -30,11 +70,11 @@ $third = $_POST["third"];
 $query = "SELECT * FROM test1 p WHERE p.uid = $id";
 $results = mysql_query($query);
 
-echo "$first";
+
 
 if (mysql_num_rows($results) == "")
 {
-	echo "noresults";
+
 	$sql1 = "INSERT INTO test1
 	VALUES
 	('$id', '$first', '$second', '$third')";
@@ -43,7 +83,7 @@ if (mysql_num_rows($results) == "")
 
 else
 {
-	echo "111";
+
 	$sql2 = sprintf("UPDATE test1 SET first_pref = '%s', second_pref = '%s', third_pref = '%s' WHERE uid = '%d'", $first, $second, $third, $id);
 	mysql_query($sql2);
 //	if (!mysql_query($con, $sql2))
